271bba492ba8b8c9ef36d8b1ab2d304f9c09502a
Matthew Elder <matt@mattelder.org>
2009-11-24 14:23:13 -0800

    added timestamp to packet

--- src/Types.hs
+++ src/Types.hs
@@ -1,9 +1,11 @@
 module Types where
 import Data.ByteString
+import Data.Time
 
 data Packet =
     Packet {
       pktId   :: Int,
       pktSize :: Int,
+      pktTimestamp :: UTCTime,
       pktRaw  :: ByteString
     }
